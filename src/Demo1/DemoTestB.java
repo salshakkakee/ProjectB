77055ef61bad18e9fd2b2f1c226f8d6cacbc0940
Sally <sally.ayad992@gmail.com>
2025-01-31 01:54:42 -0500

    Update DemoTestB.java

--- src/Demo1/DemoTestB.java
+++ src/Demo1/DemoTestB.java
@@ -3,6 +3,7 @@ package Demo1;
 public class DemoTestB {
 	public static void main(String[] args) {
 		getData("Sally", 31, 'F', "Tampa", true, 50.00);
+		getData("Zaid", 36, 'M', "Tampa", true, 70.00);
 	}
 
 	public static void getData(String name, int age, char gender, String city, boolean isCitizen, double salary) {

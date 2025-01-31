7dae9435672722daa7b2018bad858e114e87740f
salsh <salsh@Sally.lan>
2025-01-31 01:52:37 -0500

    Sally's information

---
+++ src/Demo1/DemoTestB.java
@@ -0,0 +1,18 @@
+package Demo1;
+
+public class DemoTestB {
+	public static void main(String[] args) {
+		getData("Sally", 31, 'F', "Tampa", true, 50.00);
+	}
+
+	public static void getData(String name, int age, char gender, String city, boolean isCitizen, double salary) {
+
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(gender);
+		System.out.println(city);
+		System.out.println(isCitizen);
+		System.out.println(salary);
+
+	}
+}
